a284478f0ab8feefef52a1a39237a59496a4dfcd
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2024-09-27 18:25:51 +0200

    Add ATMSimulator withdraw case

--- ATMSimulator/ATMSimulator.c
+++ ATMSimulator/ATMSimulator.c
@@ -24,16 +24,37 @@ int main(int argc, char *argv[])
         printf("Your balance is: %f\n", balance);
         break;
     case 2:
-        printf("Salut gamin !");
+        printf("\n* WITHDRAW *\n\n");
+
+        float withdraw = 5.0;
+
+        printf("Amount: ");
+        scanf("%f", &withdraw);
+
+        if (withdraw < 5.0)
+        {
+            printf("Error! Cannot withdraw less than 5.\n");
+        }
+        else if (withdraw > balance)
+        {
+            printf("Error! Balance is insuficient.\n");
+        }
+        else
+        {
+            printf("Done! Previous balance: %f\n", balance);
+            balance -= withdraw;
+            printf("Done! Current balance: %f\n", balance);
+        }
+
         break;
     case 3:
-        printf("Salut jeune !");
+        printf("\n* DEPOSIT *\n\n");
         break;
     case 4:
-        printf("Salut ado !");
+        printf("\n* LOGOUT *\n\n");
         break;
     default:
-        printf("Je n'ai aucune reponse pour ton age");
+        printf("* NOT VALID *\n\n");
         break;
     }
 
